6946f692a29cff5a0f05912090439b3c493b0ac6
Vadim Nifadyev <vnifadyev@gmail.com>
2020-09-09 17:26:13 +0300

    Implement user login logic

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 # Importing here is workaround to circular imports
 from app import routes
