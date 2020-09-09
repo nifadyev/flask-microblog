294a3e52eca71d92e9bb3908a07d1faf25ff7f10
Vadim Nifadyev <vnifadyev@gmail.com>
2020-09-09 18:08:18 +0300

    Add and migrate models User and Post

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 # Importing here is workaround to circular imports
-from app import routes
+from app import routes, models
