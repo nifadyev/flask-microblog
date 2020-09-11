326b30a572316d44ce82e77062a8b240edbc93cb
Vadim Nifadyev <vnifadyev@gmail.com>
2020-09-11 12:56:25 +0300

    Add About and Last seen to user profile page

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from flask import render_template, flash, redirect, url_for, request
 from app import app, db
 from app.forms import LoginForm, RegistrationForm
@@ -74,3 +75,10 @@ def user(username):
     ]
 
     return render_template('user.html', user=user, posts=posts)
+
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
