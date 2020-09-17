8e4fa090396fcd6a3ed5a648fdc66b148c5f8850
Vadim Nifadyev <vnifadyev@gmail.com>
2020-09-17 10:57:47 +0300

    Replace app usage with create_app

--- app/models.py
+++ app/models.py
@@ -1,13 +1,15 @@
 from datetime import datetime
-from time import time
 from hashlib import md5
-from app import db, login, app
-from werkzeug.security import generate_password_hash, check_password_hash
+from time import time
+from flask import current_app
 from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 import jwt
+from app import db, login
 
 
-followers = db.Table('followers',
+followers = db.Table(
+    'followers',
     db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
     db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
 )
@@ -59,24 +61,29 @@ class User(UserMixin, db.Model):
             followers, (followers.c.followed_id == Post.user_id)).filter(
                 followers.c.follower_id == self.id)
         own = Post.query.filter_by(user_id=self.id)
-
         return followed.union(own).order_by(Post.timestamp.desc())
 
     def get_reset_password_token(self, expires_in=600):
         return jwt.encode(
             {'reset_password': self.id, 'exp': time() + expires_in},
-            app.config['SECRET_KEY'],
+            current_app.config['SECRET_KEY'],
             algorithm='HS256'
         ).decode('utf-8')
 
     @staticmethod
     def verify_reset_password_token(token):
         try:
-            id = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])['reset_password']
+            id = jwt.decode(
+                token, current_app.config['SECRET_KEY'], algorithms=['HS256'])['reset_password']
         except:
             return
 
         return User.query.get(id)
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 
 class Post(db.Model):
@@ -88,8 +95,3 @@ class Post(db.Model):
 
     def __repr__(self):
         return f'<Post {self.body}>'
-
-
-@login.user_loader
-def load_user(id):
-    return User.query.get(int(id))
