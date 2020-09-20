8f63cbb9b57608010bab964e8238756a95e6b335
Vadim Nifadyev <vnifadyev@gmail.com>
2020-09-20 12:49:38 +0300

    Implement periodic update about unread messages

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+import json
 from datetime import datetime
 from hashlib import md5
 from time import time
@@ -37,6 +38,7 @@ class User(UserMixin, db.Model):
     messages_received = db.relationship(
         'Message', foreign_keys='Message.recipient_id', backref='recipient', lazy='dynamic')
     last_message_read_time = db.Column(db.DateTime)
+    notifications = db.relationship('Notification', backref='user', lazy='dynamic')
 
     def __repr__(self):
         return f'<User {self.username}>'
@@ -85,12 +87,19 @@ class User(UserMixin, db.Model):
             return
 
         return User.query.get(id)
-    
+
     def new_messages(self):
         last_read_time = self.last_message_read_time or datetime(1900, 1, 1)
 
         return Message.query.filter_by(recipient=self).filter(
             Message.timestamp > last_read_time).count()
+
+    def add_notification(self, name, data):
+        self.notifications.filter_by(name=name).delete()
+        notification = Notification(name=name, payload_json=json.dumps(data), user=self)
+        db.session.add(notification)
+
+        return notification
 
 
 @login.user_loader
@@ -159,3 +168,14 @@ class Message(db.Model):
 
     def __repr__(self):
         return f'<Message {self.body}>'
+
+
+class Notification(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), index=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    timestamp = db.Column(db.Float, index=True, default=time)
+    payload_json = db.Column(db.Text)
+
+    def get_data(self):
+        return json.loads(str(self.payload_json))
